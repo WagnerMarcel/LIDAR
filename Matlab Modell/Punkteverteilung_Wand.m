0571bc16a5e43104b7a9896ab0ed69626ee9344f
Marcel Wagner <marcel_wagner_@arcor.de>
2018-10-23 16:37:27 +0200

    Matlab Modell Fertiggestellt
    Kann nun eine 6m x 3m Wand bei welcher im Abstand von 2,5m der Sensor aufgebaut wird. Die Entstehende Punktewolke auf der Wand simulieren. Verschiedene Parametereinstellungen ergeben verschiedene Punkteverteilungen

--- Matlab Modell/Punkteverteilung_Wand.m
+++ Matlab Modell/Punkteverteilung_Wand.m
@@ -1,5 +1,7 @@
 %% Matlab Modell zur Bestimmung von Punkteverteilung eines LIDAR Systems
 
+clear all
+
 %% Raumbedingungen
 L_l = 600; % Länge der Wand in cm
 L_h = 300; % Höhe der Wand in cm
@@ -10,14 +12,14 @@ A_wand = L_l*L_h; % Fläche der langen Wand
 Verhaeltnis = A_wand/A_ges; % Verhälnis Fläche der langen Wand zu Gesamtfläche
 
 W_l_l = (2*atan((L_l/2)/(L_b/2))*360)/(2*pi); % Winkel, in welchem die Wand horizontal ist
-W_l_h = (atan((L_h/2)/(L_b/2))*360)/(2*pi); % Winkel, in welchem die Wand vertikal ist
+W_l_h = (atan((L_h)/(L_b/2))*360)/(2*pi); % Winkel, in welchem die Wand vertikal ist
 
 %% Zeitbedingungen
 T_max = 3600; % Maximale Messdauer in Sekunden
 
 %% Sensor-/Mechanikbedingungen
 F_sensor = 50; % Frequenz des Sensors
-Microsteps = 16; % Microsteps des Schrittmotortreibers
+Microsteps = 4; % Microsteps des Schrittmotortreibers
 Schrittmotor_winkel = 1.8;
 Schrittmotor_kleinste_Aufloesung = Schrittmotor_winkel/16;
 
@@ -33,25 +35,18 @@ W_v = floor((180/S_v)/Schrittmotor_kleinste_Aufloesung)*Schrittmotor_kleinste_Au
 S_v_w = floor(W_l_h/W_v); % Vertikale Schritte auf der Wand
 P_w = P_t * Verhaeltnis; % Punkte auf der Wand
 
-
 %% Berechnung
-E_x=zeros(S_h_w/2,S_v_w);
-E_z=zeros(S_h_w/2,S_v_w);
+E_x=zeros(S_h_w,S_v_w); % Initialisieren der Matrix für die X-Werte
+E_z=zeros(S_h_w,S_v_w); % Initialisieren der Matrix für die Z-Werte
 
-for i = 1:1:(S_h_w/2)
+figure();
+hold on; % Festhalten der Werte
+for i = -(S_h_w/2):1:(S_h_w/2)
     for j = 1:1:S_v_w
-        E_x(i,j)=tan(i*W_h)*L_b/2;
-        E_z(i,j)=tan(j*W_v)*(L_b/(2*cos(i*W_h)));
+        E_x(i+1+(S_h_w/2),j)=tan(deg2rad(i*W_h))*L_b/2; % Berechnung der X-Werte
+        E_z(i+1+(S_h_w/2),j)=tan(deg2rad(j*W_v))*(L_b/(2*cos(deg2rad(i*W_h)))); % Berechnung der Z-Werte
+        plot(E_x(i+1+(S_h_w/2),j),E_z(i+1+(S_h_w/2),j), '*r'); % Zeichnen der Punkte
     end
 end
+axis([-L_l/2 L_l/2 0 L_h]); % Einstellen der Achsen
 
-
-
-%% Plot
-for i = 1:1:(S_h_w/2)
-    for j = 1:1:S_v_w
-        plot(E_z(i,j),E_x(i,j), '*r');
-        hold on;
-        axis([0 L_l/2 0 L_h]);
-    end
-end
