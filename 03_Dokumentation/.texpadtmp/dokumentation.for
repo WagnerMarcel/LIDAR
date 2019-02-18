faecd21a9e15297f6349e898b9c2615fb86dc647
Marcel Wagner <marcel_wagner_@arcor.de>
2019-02-18 08:57:12 +0100

    Kapitel Mechanik fertig

--- 03_Dokumentation/.texpadtmp/dokumentation.for
+++ 03_Dokumentation/.texpadtmp/dokumentation.for
@@ -9,7 +9,7 @@
 \defcounter {refsection}{0}\relax 
 \contentsline {formel}{\numberline {2.1} Berechnung der Entfernung mittels Lichtlaufzeit}{5}{equation.2.1.1}
 \defcounter {refsection}{0}\relax 
-\addvspace {10\p@ }
+\contentsline {formel}{\numberline {2.2} Berechnung der minimalen und maximalen Zeit}{6}{equation.2.1.2}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
@@ -17,10 +17,20 @@
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {5.1} Berechnung \IeC {\"U}bersetzungsverh\IeC {\"a}ltnis \cite {Tabellenbuch}}{14}{equation.5.2.1}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {8.1} Umrechung Kugelkoordinaten in kartesische Koordinaten}{32}{equation.8.2.1}
+\contentsline {formel}{\numberline {8.1} Umrechung Kugelkoordinaten in kartesische Koordinaten}{35}{equation.8.2.1}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {9.1} Berechnung horizontiale Aufl\IeC {\"o}sung}{43}{equation.9.2.1}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {9.2} Berechnung vertikale Aufl\IeC {\"o}sung}{43}{equation.9.2.2}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
