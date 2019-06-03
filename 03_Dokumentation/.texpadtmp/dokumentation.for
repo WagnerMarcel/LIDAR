bfb9d6be07488f68b9ea788930d56cc87a061f02
Marcel Wagner <marcel_wagner_@arcor.de>
2019-06-03 16:13:17 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ 03_Dokumentation/.texpadtmp/dokumentation.for
@@ -0,0 +1,61 @@
+<<<<<<< Updated upstream
+=======
+\defcounter {refsection}{0}\relax 
+\select@language {ngerman}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {2.1} Position des Lichtpunkts einer \ac {PSD}}{5}{equation.2.1.1}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {3.1} Berechnung der Entfernung mittels Lichtlaufzeit}{8}{equation.3.1.1}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {3.2} Berechnung der minimalen und maximalen Zeit}{9}{equation.3.1.2}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {3.3} Zur\IeC {\"u}ckgelegte Strecke des Lichtstrahls}{10}{equation.3.2.3}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {3.4} Zur\IeC {\"u}ckgelegte Strecke des Lichtstrahls}{11}{equation.3.2.4}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {3.5} Beispielrechnung Messgenauigkeit}{12}{equation.3.2.5}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {3.6} Sinussatz}{13}{equation.3.3.6}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {3.7} Beispielrechnung Trigonometrie}{14}{equation.3.3.7}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {7.1} Berechnung \IeC {\"U}bersetzungsverh\IeC {\"a}ltnis \cite {Tabellenbuch}}{31}{equation.7.2.1}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {8.1} Berechnung maximaler Strom f\IeC {\"u}r Schrittmotoren \cite {A4988}}{39}{equation.8.2.1}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {10.1} Umrechnung Kugelkoordinaten in kartesische Koordinaten}{58}{equation.10.2.1}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {11.1} Berechnung horizontiale Aufl\IeC {\"o}sung}{68}{equation.11.2.1}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {11.2} Berechnung vertikale Aufl\IeC {\"o}sung}{68}{equation.11.2.2}
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+>>>>>>> Stashed changes
