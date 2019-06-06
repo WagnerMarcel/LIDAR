b087aeaca902fd8f7b0d627a1da3f4858cbe29cd
Marcel Wagner <marcel_wagner_@arcor.de>
2019-06-06 22:08:07 +0200

    Formatierung

--- 03_Dokumentation/.texpadtmp/dokumentation.for
+++ 03_Dokumentation/.texpadtmp/dokumentation.for
@@ -21,7 +21,7 @@
 \defcounter {refsection}{0}\relax 
 \contentsline {formel}{\numberline {3.5} Beispielrechnung Messgenauigkeit}{12}{equation.3.2.5}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {3.6} Sinussatz}{13}{equation.3.3.6}
+\contentsline {formel}{\numberline {3.6} Sinussatz}{14}{equation.3.3.6}
 \defcounter {refsection}{0}\relax 
 \contentsline {formel}{\numberline {3.7} Beispielrechnung Trigonometrie}{14}{equation.3.3.7}
 \defcounter {refsection}{0}\relax 
@@ -31,25 +31,25 @@
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {6.1} Berechnung \IeC {\"U}bersetzungsverh\IeC {\"a}ltnis \cite {Tabellenbuch}}{30}{equation.6.2.1}
+\contentsline {formel}{\numberline {6.1} Berechnung \IeC {\"U}bersetzungsverh\IeC {\"a}ltnis \cite {Tabellenbuch}}{31}{equation.6.2.1}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {7.1} Berechnung maximaler Strom f\IeC {\"u}r Schrittmotoren \cite {A4988}}{39}{equation.7.2.1}
+\contentsline {formel}{\numberline {7.1} Berechnung maximaler Strom f\IeC {\"u}r Schrittmotoren \cite {A4988}}{40}{equation.7.2.1}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {9.1} Umrechnung Kugelkoordinaten in kartesische Koordinaten}{59}{equation.9.2.1}
+\contentsline {formel}{\numberline {9.1} Umrechnung Kugelkoordinaten in kartesische Koordinaten}{60}{equation.9.2.1}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {10.1} Berechnung horizontiale Aufl\IeC {\"o}sung}{70}{equation.10.2.1}
+\contentsline {formel}{\numberline {10.1} Berechnung horizontiale Aufl\IeC {\"o}sung}{71}{equation.10.2.1}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {10.2} Berechnung vertikale Aufl\IeC {\"o}sung}{71}{equation.10.2.2}
+\contentsline {formel}{\numberline {10.2} Berechnung vertikale Aufl\IeC {\"o}sung}{72}{equation.10.2.2}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {10.3} Berechnung horizontale reale Aufl\IeC {\"o}sung}{75}{equation.10.3.3}
+\contentsline {formel}{\numberline {10.3} Berechnung horizontale reale Aufl\IeC {\"o}sung}{76}{equation.10.3.3}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
